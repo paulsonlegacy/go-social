37e6cf0f7d92487d64fe8355e3d103c2de34b910
Paulson Bosah <paulsonbosah@gmail.com>
2025-03-11 13:10:21 +0100

    Environment variables commit

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,12 +2,42 @@ package main
 
 import (
 	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/paulsonlegacy/go-env-loader"
 )
 
-func main() {
-	configuration := config{
-		address: ":8080",
+// VARIABLES
+
+var BASE_PATH string
+
+
+// FUNCTIONS
+
+func init() {
+	dir, err := os.Getwd()
+	if err != nil {
+		panic(err)
 	}
+	BASE_PATH = dir // Sets the root directory dynamically
+}
+
+
+// APPLICATION ENTRY POINT
+
+func main() {
+	envPath := filepath.Join(BASE_PATH, "internal/env/.env") // ENV file path
+
+	server_address, err := envloader.GetEnv(envPath, "SERVER_ADDRESS")
+	if err != nil {
+		log.Fatal("Error loading .env file:", err)
+	}
+
+	configuration := config{
+		address: server_address,
+	}
+
 	app := &application{
 		config: configuration,
 	}
--- go.mod
+++ go.mod
@@ -1,5 +1,8 @@
-module github.com/paulsonlegacy/gosocial
+module github.com/paulsonlegacy/go-social
 
 go 1.23.2
 
-require github.com/go-chi/chi/v5 v5.2.1 // indirect
+require (
+	github.com/go-chi/chi/v5 v5.2.1
+	github.com/paulsonlegacy/go-env-loader v0.1.0
+)
